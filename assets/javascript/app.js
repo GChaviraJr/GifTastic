307cb8932c467312a15a91e2b4fa1506f966e57f
Lupe <lupec_se@yahoo.com>
2018-11-08 18:13:07 -0700

    Minor changes to JS in var naming and I completely styled the page with minor changes to HTML to do so

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
-    var gifs = ["Charmander", "Pikachu"];
+    var topic = ["Pikachu", "Squirtle", "Charmander", "Bulbasaur", "Mewtwo", "Pidgey"];
 
     $("#buttons-view").on("click", ".gif-btn", function () {
 
-        var searchGif = $("button").attr("data-name");
+        var searchGif = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=dm4KqCcGOV1oFmA03pKV7WESYtRsWsxt&q=" + searchGif + "&limit=10&offset=0&lang=en";
 
         $.ajax({
@@ -15,30 +15,33 @@ $(document).ready(function () {
                 var gifDiv = $("<div class='gif'>");
                 var rating = response.data[index].rating;
                 var imageUrl = response.data[index].images.original.url;
-                $('<img src="' + imageUrl + '" />' + "<br/>" + "<p>Rating: " + rating + "</p>").appendTo('#images');
-                $("#images").prepend(gifDiv);
+                $('<img src="' + imageUrl + '"height="200px" />' + "<br/>" + "<p>Rating: " + rating + "</p>").appendTo('#images');
+                $("#images").append(gifDiv);
             })
         });
     });
 
     function renderButtons() {
         $("#buttons-view").empty();
-        for (var i = 0; i < gifs.length; i++) {
+        for (var i = 0; i < topic.length; i++) {
             var a = $("<button>");
             a.addClass("gif-btn");
-            a.addClass("gif");
-            a.attr("data-name", gifs[i]);
-            a.text(gifs[i]);
-            $("#buttons-view").append(a);
+            a.attr("data-name", topic[i]);
+            a.text(topic[i]);
+            $("#buttons-view").prepend(a);
         }
     }
 
     $("#add-gif").on("click", function (event) {
         event.preventDefault();
         var addGif = $("#gif-input").val().trim();
-        gifs.push(addGif);
+        topic.push(addGif);
         renderButtons();
     });
+
+    $("#resetButton").on("click", function (event) {
+        $("<img>").remove();
+    })
 
     renderButtons()
 
